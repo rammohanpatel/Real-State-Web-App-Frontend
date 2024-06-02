83c78b632a77aada0f22ddc1bd64dbca2a029e3a
Ram Mohan <rammohan@Mehirrs-MacBook-Air.local>
2024-06-03 00:32:22 +0530

    latest

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -51,49 +51,49 @@ const Contact = () => {
   return (
     <section className="bg-white mt-14">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 ">Contact Us</h2>
-        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500  sm:text-xl">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contact Us</h2>
+        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">
           Please note: while we appreciate your questions, we are unable to respond to all inquiries. To learn more, visit how we help clients.
         </p>
-        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500  sm:text-xl">
+        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">
           Required fields are marked with an asterisk (*).
         </p>
         <form onSubmit={handleSubmit} className="space-y-8">
           <div>
-            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 ">Your Name*</label>
-            <input type="text" id="name" value={formData.name} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " required />
+            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Your Name*</label>
+            <input type="text" id="name" value={formData.name} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" required />
           </div>
           <div>
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Email Address*</label>
-            <input type="email" id="email" value={formData.email} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " required />
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email Address*</label>
+            <input type="email" id="email" value={formData.email} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" required />
           </div>
           <div>
-            <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-900 ">Company*</label>
-            <input type="text" id="company" value={formData.company} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " required/>
+            <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-900">Company*</label>
+            <input type="text" id="company" value={formData.company} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" required />
           </div>
           <div>
-            <label htmlFor="position" className="block mb-2 text-sm font-medium text-gray-900 ">Position*</label>
-            <input type="text" id="position" value={formData.position} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " required />
+            <label htmlFor="position" className="block mb-2 text-sm font-medium text-gray-900">Position*</label>
+            <input type="text" id="position" value={formData.position} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" required />
           </div>
           <div>
-            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 ">Message*</label>
-            <textarea id="message" value={formData.message} onChange={handleChange} rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " required></textarea>
+            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Message*</label>
+            <textarea id="message" value={formData.message} onChange={handleChange} rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" required></textarea>
           </div>
           <div>
-            <label htmlFor="street" className="block mb-2 text-sm font-medium text-gray-900 ">Street Address</label>
-            <input type="text" id="street" value={formData.street} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " />
+            <label htmlFor="street" className="block mb-2 text-sm font-medium text-gray-900">Street Address</label>
+            <input type="text" id="street" value={formData.street} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" />
           </div>
           <div>
-            <label htmlFor="city" className="block mb-2 text-sm font-medium text-gray-900 ">City and State/Region</label>
-            <input type="text" id="city" value={formData.city} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " />
+            <label htmlFor="city" className="block mb-2 text-sm font-medium text-gray-900">City and State/Region</label>
+            <input type="text" id="city" value={formData.city} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" />
           </div>
           <div>
-            <label htmlFor="postal" className="block mb-2 text-sm font-medium text-gray-900 ">Postal Code</label>
-            <input type="text" id="postal" value={formData.postal} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " />
+            <label htmlFor="postal" className="block mb-2 text-sm font-medium text-gray-900">Postal Code</label>
+            <input type="text" id="postal" value={formData.postal} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" />
           </div>
           <div>
-            <label htmlFor="country" className="block mb-2 text-sm font-medium text-gray-900 ">Country</label>
-            <input type="text" id="country" value={formData.country} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " />
+            <label htmlFor="country" className="block mb-2 text-sm font-medium text-gray-900">Country</label>
+            <input type="text" id="country" value={formData.country} onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" />
           </div>
           <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300">Submit</button>
         </form>
--- src/lib/client.js
+++ src/lib/client.js
@@ -6,5 +6,5 @@ export const client = createClient({
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
     useCdn: true,
-    token: process.env.SANITY_API_TOKEN
+    token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN
 })
